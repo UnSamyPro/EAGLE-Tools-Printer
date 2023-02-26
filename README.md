f38abdccbc6d264e3b3f56a53195a1ee15d65033
UnSamyPro <40127757+UnSamyPro@users.noreply.github.com>
2023-02-26 17:02:14 +0100

    Add cam files; Add README.md; Bump version

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # EAGLE-Tools-Printer
 
+
+## Introduction
+
+EAGLE-Tools-Printer is a tool that simplifies the process of exporting PDFs with predefined configurations in the EAGLE PCB design software. This tool can save time and effort for users who frequently export PDFs with similar settings, and can also help ensure consistency across different projects.
+
+
+## Installation
+
+To install or update EAGLE-Tools-Printer, run the following command in the console:
+
+```BATCH
 curl -sSL https://raw.githubusercontent.com/UnSamyPro/EAGLE-Tools-Printer/master/install.cmd -o install.bat && cmd /c install.bat && del install.bat
+```
+<br>
+
+Alternatively, you can manually install the tool by following these steps:
+- Copy the files in the `cam` folder to `<EAGLEDIR>\cam`
+- Copy the files `_EAGLE-Tools_cam2print.ulp` and `_EAGLE-Tools-Printer.ulp` to `<EAGLEDIR>\ulp`
+- Copy `eagle.src` to `<EAGLEDIR>\src`
+- Then restart EAGLE
+
+
+## License
+
+This project is licensed under the terms of the MIT license. See the [LICENSE](LICENSE) file for details.
--- install.cmd
+++ install.cmd
@@ -1,3 +1,25 @@
+:: MIT License
+:: 
+:: Copyright (c) 2023 SamyPro (Samuel Prossliner)
+:: 
+:: Permission is hereby granted, free of charge, to any person obtaining a copy
+:: of this software and associated documentation files (the "Software"), to deal
+:: in the Software without restriction, including without limitation the rights
+:: to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+:: copies of the Software, and to permit persons to whom the Software is
+:: furnished to do so, subject to the following conditions:
+:: 
+:: The above copyright notice and this permission notice shall be included in all
+:: copies or substantial portions of the Software.
+:: 
+:: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+:: IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+:: FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+:: AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+:: LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+:: OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+:: SOFTWARE.
+
 @ECHO OFF
 
 SET originalFolder=%CD%
@@ -116,6 +138,11 @@ CD %filename%\src
 MOVE /Y "_EAGLE-Tools_cam2print.ulp" "%FOLDER%\ulp"
 MOVE /Y "_EAGLE-Tools-Printer.ulp" "%FOLDER%\ulp"
 MOVE /Y "eagle.scr" "%FOLDER%\scr"
+MOVE /Y "_Bestueckungsseite_Film.cam" "%FOLDER%\cam"
+MOVE /Y "_Bestueckungsseite.cam" "%FOLDER%\cam"
+MOVE /Y "_Leiterbahnseite_Film.cam" "%FOLDER%\cam"
+MOVE /Y "_Leiterbahnseite.cam" "%FOLDER%\cam"
+MOVE /Y "_Schaltplan.cam" "%FOLDER%\cam"
 
 ECHO Cleaning up...
 :: delete the temp folder

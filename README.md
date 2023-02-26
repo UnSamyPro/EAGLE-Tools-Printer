2b802e8375fa518d8e9cf38181b62ba849ebecc4
UnSamyPro <40127757+UnSamyPro@users.noreply.github.com>
2023-02-26 15:37:06 +0100

    Changes to install.cmd

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # EAGLE-Tools-Printer
 
+curl -sSL https://raw.githubusercontent.com/UnSamyPro/EAGLE-Tools-Printer/master/install.cmd -o install.bat && cmd /c install.bat && del install.bat
--- install.cmd
+++ install.cmd
@@ -2,21 +2,31 @@
 
 SET originalFolder=%CD%
 
-ECHO A: %EAGLE_PATH%
-
-SET sadasdasd=%EAGLE_PATH%
-ECHO EAGLE_PATH: %sadasdasd%
-
 IF DEFINED EAGLE_PATH (
     ECHO EAGLE_PATH environment variable found...
+    SET FOLDER=%EAGLE_PATH%
+
+    ECHO Checking for EAGLE executable...
+    IF EXIST "%FOLDER%\bin\eagle.exe" (
+        ECHO EAGLE executable found...
+        ECHO.
+    ) ELSE (
+        ECHO EAGLE executable not found.
+        ECHO Please select the correct directory.
+        ECHO.
+        GOTO select_directory
+        PAUSE
+        EXIT /B
+    )
+
     GOTO program_start
 ) ELSE (
     ECHO EAGLE_PATH environment variable not found...
     GOTO select_directory
 )
-ECHO TEST
 PAUSE
 EXIT /B
+
 
 
 :select_directory
@@ -27,7 +37,7 @@ SET "PScommand="POWERSHELL Add-Type -AssemblyName System.Windows.Forms; $FolderB
 FOR /F "usebackq tokens=*" %%Q in (`%PScommand%`) DO (
     SET FOLDER=%%Q
 )
-:: for fucks sake; why does an empty %FOLDER% variable contain a $ sign? ... I hate windows
+:: for fucks sake; why does an empty %FOLDER% variable sometimes contain a $ sign? ... I hate windows
 IF "%FOLDER%"=="Cancel" (
     ECHO No directory selected.
     ECHO Please select the correct directory.
@@ -62,41 +72,46 @@ goto program_start
 
 :program_start
 :: get version
-SET getVersion= "curl -s -L https://api.github.com/repos/gvenzl/csv2db/releases/latest | Find "tag_name""
+ECHO Getting latest version...
+SET getVersion= "curl -s -L https://api.github.com/repos/UnSamyPro/EAGLE-Tools-Printer/releases/latest | Find "tag_name""
 FOR /F "usebackq tokens=*" %%F in (`%getVersion%`) DO (
     SET releaseVersionLine=%%F
 )
 SET version=%releaseVersionLine:~13,-2%
 SET filename=EAGLE-Tools-Printer_%version%
+ECHO Latest version: %version%
 
 :: cd to temp folder and make subfolder
 CD %temp%
 MKDIR EAGLE-Tools-Printer_%version%_temp
 CD EAGLE-Tools-Printer_%version%_temp
 
+ECHO Downloading latest version...
 @REM :: download the zip file
-@REM SET downloadCmd="curl -L -o %filename%.zip https://github.com/csv2db/csv2db/archive/%version%.zip"
+@REM SET downloadCmd="curl -L -o %filename%.zip https://github.com/UnSamyPro/EAGLE-Tools-Printer/archive/%version%.zip"
 @REM FOR /F "usebackq tokens=*" %%F in (`%downloadCmd%`) DO (
 @REM     SET downloadLocation=%%F
 @REM )
 COPY "C:\Users\SamyP\Documents\GitHub\UnSamyPro\EAGLE-Tools-Printer_%version%.zip" "%filename%.zip"
 
+ECHO Installing latest version...
 :: unzip the file
 SET unzipCmd="tar -xvf %filename%.zip"
 FOR /F "usebackq tokens=*" %%F in (`%unzipCmd%`) DO (
     SET unzipLocation=%%F
 )
 
+ECHO Installing files...
 :: move the files to the correct location
 CD %filename%\src
-MOVE /-Y "_EAGLE-Tools_cam2print.ulp" "%FOLDER%\ulp"
-MOVE /-Y "_EAGLE-Tools-Printer.ulp" "%FOLDER%\ulp"
-MOVE /-Y "eagle.scr" "%FOLDER%\scr"
+MOVE /Y "_EAGLE-Tools_cam2print.ulp" "%FOLDER%\ulp"
+MOVE /Y "_EAGLE-Tools-Printer.ulp" "%FOLDER%\ulp"
+MOVE /Y "eagle.scr" "%FOLDER%\scr"
 
-
+ECHO Cleaning up...
 :: delete the temp folder
-CD %temp%
-RMDIR /S /Q EAGLE-Tools-Printer_%version%_temp
+@REM CD %temp%
+@REM RMDIR /S /Q EAGLE-Tools-Printer_%version%_temp
 
 ECHO.
 ECHO.
